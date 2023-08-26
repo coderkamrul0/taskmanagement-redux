deabfc1b5f4b92eeadd235b65225c73467fc8ac2
Kamrul Hasan <kamrul444216@gmail.com>
2023-08-27 03:47:19 +0600

    Redux setup

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import taskSlice from "./features/tasks/taskSlice";
+
+
+const store = configureStore({
+    reducer: {
+        task: taskSlice,
+    },
+})
+
+export default store;
